9fa17fee30c5431bf3a2e5297c7e636e7d5588dc
Yakov <167911620+GitW1n@users.noreply.github.com>
2024-12-01 18:51:38 +0300

    Update app.py

--- Weather-app/app.py
+++ Weather-app/app.py
@@ -4,3 +4,17 @@ import requests
 def main(page):
     page.title = "Weather"
     page.theme_mode = 'dark'
+
+    user_data = ft.TextField(label='Введите город', width=400)
+    result_text = ft.Text(value='', size=16, color='white')
+
+    def get_info(e):
+        city = user_data.value
+        if not city:
+            result_text.value = "Пожалуйста, введите название города."
+            page.update()
+            return
+        
+        API = 'f91a2b525491521109dd34d4f8132567'
+        URL = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric'
+
